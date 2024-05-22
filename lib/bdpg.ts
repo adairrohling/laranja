c6b75057368802661d11dee1dac097d8696bb13b
adairrohling <81537071+adairrohling@users.noreply.github.com>
2024-05-22 18:39:23 -0300

    Update connections

--- lib/bdpg.ts
+++ lib/bdpg.ts
@@ -6,7 +6,15 @@ import { Pool, Client } from "pg";
 const connectionString =
     "postgres://default:EQ56KwbDjNVl@ep-throbbing-pine-12654090.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
 export const pool = new Pool({
-  connectionString,
+  connectionString,,
+  max: 10, // Número máximo de clientes no pool
+  idleTimeoutMillis: 30000, // Tempo de inatividade antes de uma conexão ser fechada
+  connectionTimeoutMillis: 6000, // Tempo máximo para tentar conectar antes de falhar
+});
+
+pool.on('error', (err, client) => {
+  console.error('Erro inesperado no cliente do pool', err);
+  process.exit(-1);
 });
 
 export const client = new Client({
